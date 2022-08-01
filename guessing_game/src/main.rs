4cfe6d90c81afe7ea5d2ce04e35ea5232a6791d0
xpanvictor <xpanvictor@gmail.com>
2022-08-01 20:56:58 +0100

    Taking user input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // introducing the game
+    println!("Guess the number!");
+    println!("Please input the number: ");
+
+    // creating a mutable variable with `mut`; without which it'll be immutable
+    let mut guess = String::new();
+
+    // stdin() instantiates the std library for io operations
+    io::stdin()
+        // this takes input from terminal and pipes it to the mutable reference of the guess var
+        .read_line(&mut guess)
+        // handle error also returns either Ok with value of read or Err with trace
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
 }
