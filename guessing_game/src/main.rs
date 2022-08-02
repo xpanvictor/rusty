b0a93899b507abea3a97195aba60bff730a9dc45
xpanvictor <xpanvictor@gmail.com>
2022-08-02 11:39:24 +0100

    Game logic n loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -7,19 +8,37 @@ fn main() {
     let secret_number = rand::thread_rng()
         // generate random num between range start..=end
         .gen_range(1..=100);
-    // introducing the game
+
     println!("Guess the number!");
-    println!("Please input the number: ");
+    // the loop system
+    loop {
+        // introducing the game
+        println!("Please input the number: ");
 
-    // creating a mutable variable with `mut`; without which it'll be immutable
-    let mut guess = String::new();
+        // creating a mutable variable with `mut`; without which it'll be immutable
+        let mut guess = String::new();
 
-    // stdin() instantiates the std library for io operations
-    io::stdin()
-        // this takes input from terminal and pipes it to the mutable reference of the guess var
-        .read_line(&mut guess)
-        // handle error also returns either Ok with value of read or Err with trace
-        .expect("Failed to read line");
+        // stdin() instantiates the std library for io operations
+        io::stdin()
+            // this takes input from terminal and pipes it to the mutable reference of the guess var
+            .read_line(&mut guess)
+            // handle error also returns either Ok with value of read or Err with trace
+            .expect("Failed to read line");
 
-    println!("You guessed: {guess}");
+        // convert guess string to unsigned 32b integer
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        println!("You guessed: {guess}");
+
+        // match => switch and Ordering => case<enum> ref js
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                // break the loop
+                break;
+            }
+        }
+    }
 }
