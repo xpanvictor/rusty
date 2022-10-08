9aab238f016c35bfbe85664895ba0f8762d3be9d
xpanvictor <xpanvictor@gmail.com>
2022-10-08 19:54:51 +0100

    Refr rect with structs

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -31,17 +31,36 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
 impl Rectangle {
     fn area(&self) -> u32 {
-        self.height * self.width
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle { width: size, height: size }
     }
 }
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50
     };
+
+    let rect2 = Rectangle {
+        width: 20,
+        height: 20
+    };
+
+    let square1 = Rectangle::square(64);
     // saying rust print the rect1 struct using debug format, which needs an outer attribute
     println!("The rectangle is {:#?}", rect1);
     println!("The area of the rectangle is {} square pixels.", rect1.area());
+
+    println!("Rectangle 1 can {} hold rectangle 2", if rect1.can_hold(&rect2) {"definitely"} else {"not"});
+    println!("Rectangle 1 can {} hold square 1", if rect1.can_hold(&square1) {"definitely"} else {"not"});
 }
