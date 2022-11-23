c334164afbb6b73349b83e8f74bb79d4808cf809
xpanvictor <xpanvictor@gmail.com>
2022-11-23 19:18:09 +0100

    Ft: vectors intro, creating n adding to it

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    // one way of declaring vectors by instantiating without values
+    // let v: Vec<i32> = Vec::new();
+    // another way by using the vec macro and just putting the values
+    let mut v = vec![1, 2, 3, 4];
+    // we use push to add elements to vectors; NOTE vectors r stored on heap unlike arrays
+    v.push(5);
+    v.push(6);
+    // accessing the item
+    let third: &i32 = &v[2];
+    println!("The element at index 2 is {}", third);
+
+    match v.get(3) {
+        Some(elem) => println!("Element at index 4 is {}", elem),
+        None => println!("There is no third element")
+    }
 }
