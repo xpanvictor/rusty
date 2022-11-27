6f22edf1d9f9e00feb7e34d62b46c71e1bf90d6d
xpanvictor <xpanvictor@gmail.com>
2022-11-27 11:15:32 +0100

    Feat: finished hashmaps hence completed collections chapter

--- collections/src/main.rs
+++ collections/src/main.rs
@@ -1,3 +1,4 @@
+
 fn main() {
     // one way of declaring vectors by instantiating without values
     // let v: Vec<i32> = Vec::new();
@@ -59,5 +60,37 @@ fn main() {
     let s3 = String::from("toe");
 
     let s = format!("{}-{}-{}", s1, s2, s3);
+
+    // hash maps
+    use std::collections::HashMap;
+    // creating a new hashmap
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    println!("{:#?}", scores);
+
+    // using iterator tuples to construct hashmaps from vectors with zip function
+    let teams = vec![String::from("Blue"), String::from("Yellow")];
+    let initial_scores = vec![10, 50];
+
+    let mut scoresHash: HashMap<_, _> = teams.into_iter().zip(initial_scores.into_iter()).collect();
+    let name = String::from("Blue");
+    println!("The blue's score is {:?}", scoresHash.get(&name));
+    
+    // checking if an hashmap has a key else update it
+    scoresHash.entry(String::from("Yellow")).or_insert(300);
+    scoresHash.entry(String::from("White")).or_insert(40);
+    println!("{:#?}", scoresHash);
+
+    // using an hashmap for a word counter
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:#?}", map);
 
 }
