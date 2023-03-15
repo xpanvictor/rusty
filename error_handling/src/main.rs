90d785684e054ed2602a672eb80c38329b540774
xpanvictor <xpanvictor@gmail.com>
2023-03-15 07:27:23 +0100

    feat: reads username from file and handles error

---
+++ error_handling/src/main.rs
@@ -0,0 +1,33 @@
+use std::io::{self, Read};
+use std::fs::File;
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let file = File::open("username.txt");
+
+    let mut file = match file {
+        Ok(file) => file,
+        Err(e) => return Err(e)
+    };
+
+    let mut s = String::new();
+
+    match file.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e)
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+
+    let username_result = read_username_from_file();
+    let otp: u32 = 32849;
+    let mut merged_data = String::new();
+
+    let username = username_result.unwrap_or_else(|error| {
+        panic!("Failed at retrieving username, {:?}", error)
+    });
+
+    merged_data = format!("{} {}", username, otp);
+    println!("Your merged data is {merged_data}")
+}
