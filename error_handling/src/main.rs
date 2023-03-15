d76a96476355b76c66d32cd9634c463931436e6d
xpanvictor <xpanvictor@gmail.com>
2023-03-15 07:31:20 +0100

    refr: using ? operator

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -2,19 +2,25 @@ use std::io::{self, Read};
 use std::fs::File;
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let file = File::open("username.txt");
-
-    let mut file = match file {
-        Ok(file) => file,
-        Err(e) => return Err(e)
-    };
-
+    // ----------Normal implementation-----------
+    // let file = File::open("username.txt");
+    //
+    // let mut file = match file {
+    //     Ok(file) => file,
+    //     Err(e) => return Err(e)
+    // };
+    //
+    // let mut s = String::new();
+    //
+    // match file.read_to_string(&mut s) {
+    //     Ok(_) => Ok(s),
+    //     Err(e) => Err(e)
+    // }
+    // -----------Using the ? operator implementation--------
+    let file = File::open("username.txt")?;
     let mut s = String::new();
-
-    match file.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e)
-    }
+    file.read_to_string(&mut s)?;
+    Ok(s)
 }
 
 fn main() {
