2a7c4dab27d6d83944f68b4e356006952a663a36
xpanvictor <xpanvictor@gmail.com>
2023-03-18 15:59:52 +0100

    feat: uses the closes fn to generate default shirt color

--- closures_iterators/src/main.rs
+++ closures_iterators/src/main.rs
@@ -34,5 +34,21 @@ impl Inventory {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue]
+    };
+
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    )
 }
