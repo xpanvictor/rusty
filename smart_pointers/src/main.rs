358da39f853869a17b9a27ad6d82e1358694847a
xpanvictor <xpanvictor@gmail.com>
2023-03-21 20:23:17 +0100

    feat: using deref trait for mybox to use * operator

--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,8 +1,25 @@
+use std::ops::Deref;
 use crate::List::{Cons, Nil};
 
 enum List {
     Cons(i32, Box<List>),
     Nil
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 fn main() {
@@ -16,5 +33,9 @@ fn main() {
             ))
         ))
     );
+    let val = 5;
+    let a = MyBox::new(val);
+    assert_eq!(5, val);
+    assert_eq!(5, *a);
     println!("Hello, world!");
 }
