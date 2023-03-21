44e040052c0b1ca84722050d4b5a29efe174baf9
xpanvictor <xpanvictor@gmail.com>
2023-03-21 12:37:10 +0100

    build: changes to idea config

---
+++ add/add_one/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn add_one(left: usize) -> usize {
+    left + 1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add_one(2);
+        assert_eq!(result, 3);
+    }
+}
