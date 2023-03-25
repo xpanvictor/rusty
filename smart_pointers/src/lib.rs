2a5923ab37a167018381732f26dd64e62f50dd7c
xpanvictor <xpanvictor@gmail.com>
2023-03-25 10:42:12 +0100

    feat: using refcell to bypass immutability of property

--- smart_pointers/src/lib.rs
+++ smart_pointers/src/lib.rs
@@ -43,5 +43,35 @@ pub fn mock_int_mutability() {
 
 #[cfg(test)]
 mod tests {
+    use std::cell::RefCell;
+    use super::*;
 
+    struct MockMessenger {
+        sent_messages: RefCell<Vec<String>>,
+    }
+
+    impl MockMessenger {
+        fn new() -> MockMessenger {
+            MockMessenger {
+                sent_messages: RefCell::new(vec![]),
+            }
+        }
+    }
+
+    impl Messenger for MockMessenger {
+        fn send(&self, message: &str) {
+            self.sent_messages.borrow_mut().push(String::from(message));
+        }
+    }
+
+    #[test]
+    fn it_sends_an_over_75_warning() {
+        let mock_messenger = MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger, 100);
+
+        limit_tracker.set_value(80);
+
+        // check that mock messenger has 1 message
+        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+    }
 }
