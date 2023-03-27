832e6fb188ffbce56c2a3963602722921fe34f0a
xpanvictor <xpanvictor@gmail.com>
2023-03-27 08:47:20 +0100

    feat: uses the join method on JoinHandle to ensure spawned thread ends

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -1,3 +1,19 @@
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("Number {i} from child thread");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("Number from main {i}");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    // block main thread execution or exist until handle thread ends
+    handle.join().unwrap();
 }
