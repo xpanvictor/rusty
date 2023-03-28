09b90a29af24f1ab2f9a6e2a130e2e4d2ac9b4b4
xpanvictor <xpanvictor@gmail.com>
2023-03-28 02:24:41 +0100

    feat: oop with gui

---
+++ gui/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    components: Vec<Box<dyn Draw>>,
+}
+
+impl Screen {
+    fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
